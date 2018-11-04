35ff51febe5a94069a347c44b9393683c2b405a3
binhsonnguyen <son@binhsonnguyen.com>
2018-11-04 09:28:42 +0700

    refactor

--- test/ballot.js
+++ test/ballot.js
@@ -11,25 +11,20 @@ const A_PROPOSAL = 0
 const ANOTHER_PROPOSAL = 1
 const SECOND = 1
 const REGISTER_DURATION = 5 * SECOND
-
-let chair
-let aVoter
-let anOtherVoter
+const A_BIT_MORE = 1 * SECOND
 
 contract('ballot/registering', accounts => {
+  const CHAIR = accounts[0]
+  const A_VOTER = accounts[1]
+  const AN_OTHER_VOTER = accounts[2]
+
   let contract = null
   let register = null
   let vote = null
   let travelTime = null
 
-  before(() => {
-    chair = accounts[0]
-    aVoter = accounts[1]
-    anOtherVoter = accounts[2]
-  })
-
   beforeEach(async () => {
-    contract = await Ballot.new(accounts[chair])
+    contract = await Ballot.new(accounts[CHAIR])
     register = Register(contract)
     vote = Vote(contract)
     travelTime = TimeTraveller(contract)
@@ -37,45 +32,43 @@ contract('ballot/registering', accounts => {
 
   it('...should let chairman attempt registering', async () => {
     attempt(async () => {
-      await register(aVoter).by(chair)
+      await register(A_VOTER).by(CHAIR)
     }).should.be.succeed()
   })
 
   it('...should let chairman attempt registering in time', async () => {
     attempt(async () => {
       await travelTime(2 * SECOND)
-      await register(aVoter).by(chair)
+      await register(A_VOTER).by(CHAIR)
     }).should.be.succeed()
   })
 
   it('...should reject others registering', async () => {
     attempt(async () => {
-      await register(aVoter).by(anOtherVoter)
+      await register(A_VOTER).by(AN_OTHER_VOTER)
     }).should.be.rejected()
   })
 
   it('...should reject voter do vote in early time', async () => {
     attempt(async () => {
-      await register(aVoter).by(chair)
-      await vote(A_PROPOSAL).by(aVoter)
+      await register(A_VOTER).by(CHAIR)
+      await vote(A_PROPOSAL).by(A_VOTER)
     }).should.be.rejected()
   })
 })
 
 contract('ballot/voting', accounts => {
+  const CHAIR = accounts[0]
+  const A_VOTER = accounts[1]
+  const AN_OTHER_VOTER = accounts[2]
+
   let contract = null
   let register = null
   let vote = null
   let travelTime = null
 
-  before(() => {
-    chair = accounts[0]
-    aVoter = accounts[1]
-    anOtherVoter = accounts[2]
-  })
-
   beforeEach(async () => {
-    contract = await Ballot.new(accounts[chair])
+    contract = await Ballot.new(accounts[CHAIR])
     register = Register(contract, accounts)
     vote = Vote(contract, accounts)
     travelTime = TimeTraveller(contract)
@@ -83,25 +76,25 @@ contract('ballot/voting', accounts => {
 
   it('should reject registering when time over', async () => {
     attempt(async () => {
-      await travelTime(REGISTER_DURATION + 1)
-      await register(aVoter).by(chair)
+      await travelTime(REGISTER_DURATION + A_BIT_MORE)
+      await register(A_VOTER).by(CHAIR)
     }).should.be.rejected()
   })
 
   it('...should let registered voter do vote when in time', async () => {
     attempt(async () => {
-      await register(anOtherVoter).by(chair)
-      await travelTime(REGISTER_DURATION + 1)
-      await vote(A_PROPOSAL).by(aVoter)
+      await register(AN_OTHER_VOTER).by(CHAIR)
+      await travelTime(REGISTER_DURATION + A_BIT_MORE)
+      await vote(A_PROPOSAL).by(A_VOTER)
     }).should.be.succeed()
   })
 
   it('...should reject re-vote', async () => {
     attempt(async () => {
-      await register(aVoter).by(chair)
-      await travelTime(REGISTER_DURATION + 1)
-      await vote(A_PROPOSAL).by(aVoter)
-      await vote(ANOTHER_PROPOSAL).by(aVoter)
+      await register(A_VOTER).by(CHAIR)
+      await travelTime(REGISTER_DURATION + A_BIT_MORE)
+      await vote(A_PROPOSAL).by(A_VOTER)
+      await vote(ANOTHER_PROPOSAL).by(A_VOTER)
     }).should.be.rejected()
   })
 })
