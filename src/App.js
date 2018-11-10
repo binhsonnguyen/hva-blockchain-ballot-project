7c34ba1060357b49a280a50b18b6e6c145102c91
binhsonnguyen <son@binhsonnguyen.com>
2018-11-10 21:42:33 +0700

    connect to blockchain

--- src/App.js
+++ src/App.js
@@ -1,28 +1,64 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
+import logo from './logo.svg'
+import getWeb3 from './utils/getWeb3'
+
+import Ballot from './contracts/Ballot.json'
+import truffleContract from 'truffle-contract'
+
+import './App.css'
+
+let log = console.log
 
 class App extends Component {
-  render() {
+
+  componentDidMount = async () => {
+    try {
+      const web3 = await getWeb3()
+
+      const accounts = await web3.eth.getAccounts()
+
+      const Contract = truffleContract(Ballot)
+      Contract.setProvider(web3.currentProvider)
+      const instance = await Contract.deployed()
+
+      this.setState({web3, accounts, contract: instance}, this.runExample)
+    } catch (error) {
+      alert(
+        `Failed to load web3, accounts, or contract. Check console for details.`
+      )
+      log(error)
+    }
+  }
+
+  runExample = async () => {
+    const {accounts, contract} = this.state
+
+    let proposals = Number(await contract.proposalsCount.call())
+    log('proposal count', proposals)
+
+    this.setState({proposalsCount: proposals})
+  }
+
+  render () {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
+      <div className='App'>
+        <header className='App-header'>
+          <img src={logo} className='App-logo' alt='logo'/>
           <p>
             Edit <code>src/App.js</code> and save to reload.
           </p>
           <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
+            className='App-link'
+            href='https://reactjs.org'
+            target='_blank'
+            rel='noopener noreferrer'
           >
             Learn React
           </a>
         </header>
       </div>
-    );
+    )
   }
 }
 
-export default App;
+export default App
